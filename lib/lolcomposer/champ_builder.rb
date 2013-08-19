81d2289be3c7f9726b889c9a0f3d4518fa1ab48b
Aaron Sherrin <aaron.sherrin@gmail.com>
2013-08-19 16:24:16 -0400

    Refactor champ_builder from instance to service class

--- lib/lolcomposer/champ_builder.rb
+++ lib/lolcomposer/champ_builder.rb
@@ -1,29 +1,17 @@
 module LolComposer 
     class ChampBuilder
-        attr_accessor :roster, :champions
-        def initialize yml
-            @yml = yml
-            @roster = []
-            parse
+
+        def self.parse
+            YAML.load_file('data/champions.yml')
         end
 
-        def parse
-            @champions = YAML.load_file(@yml)
-        end
-
-        def buildOne name
-            parse
-            champ = LolComposer::Champion.new
-            champ.name = name
-            champ.lane =  @champions.fetch(name).fetch("lane")
-            champ
-        end
-
-        def build
-            @champions.each do |key, value|
-                @roster << LolComposer::Champion.new(key, value["lane"])
+        def self.build
+            champions = parse
+            roster = []
+            champions.each do |key, value|
+                roster << LolComposer::Champion.new(key, value["lane"])
             end
-            @roster
+            roster
         end
 
     end
--- spec/champbuilder_spec.rb
+++ spec/champbuilder_spec.rb
@@ -1,22 +1,14 @@
 require 'spec_helper'
 
+
 describe LolComposer::ChampBuilder do
 
-    let(:builder) {LolComposer::ChampBuilder.new 'data/champions.yml'}
-
-    it "reads the champions.yml into a hash" do
-        builder.champions
+    it "should parse the hash in champions.yml" do
+        LolComposer::ChampBuilder.parse
     end
 
-    it "builds a champion object from champions.yml" do
-        champ = builder.buildOne 'Aatrox'
-        expect(champ.name).to eq 'Aatrox'
-        #puts champ.lane
-    end
-
-    it "builds every champ" do
-        builder.build
-        #3puts builder.roster
+    it "should create a list of champions from champions.yml" do
+        LolComposer::ChampBuilder.build
     end
 
 end
